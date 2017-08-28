e4590663139380bb858d07a888eba09b71042541
gregrory <gregrory>
2017-08-28 15:16:44 +0800

    2017年08月28日15:16:41

--- transcript.js
+++ transcript.js
@@ -1,6 +1,7 @@
 let query = require('cli-interact').question;
 module.exports = function transcript(students){
-let format = query('请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：').split(',');
+
+	let format = query('请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：').split(',');
 	let transcript = '成绩单\n 姓名|数学|语文|英语|编程|平均分|总分 \n========================';
 	let sumarr = [] ;
 	for (let A of students){
@@ -9,7 +10,7 @@ let format = query('请输入要打印的学生的学号（格式： 学号, 学
 		for( let B of format ){
 			console.log(B);
 			if(AID[1].trim() ===  B){
-			transcript += '\n' + AID[0] + '|' + A.数学 + '|' + A.语文 + '|' + A.英语 + '|' + A.编程 + '|' + A.平均分 + '|' + A.总分 ;	
+				transcript += '\n' + AID[0] + '|' + A.数学 + '|' + A.语文 + '|' + A.英语 + '|' + A.编程 + '|' + A.平均分 + '|' + A.总分 ;	
 			}
 		}
 
@@ -22,7 +23,7 @@ let format = query('请输入要打印的学生的学号（格式： 学号, 学
 	else {
 		median = sumarr[ Math.floor(sumarr.length / 2)];
 	}
-	
-transcript +=  '\n========================\n' + '全班总分平均分：' + String(sum / students.length)+ '\n' + '全班总分中位数：' + String(median);
+
+	transcript +=  '\n========================\n' + '全班总分平均分：' + String(sum / students.length)+ '\n' + '全班总分中位数：' + String(median);
 	console.log(transcript);
 };
